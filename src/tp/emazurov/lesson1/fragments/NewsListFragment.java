bef433c51f5c53c191b9f5dc3ba539076322e4db
Rustam Kildiev <r.kildiev@corp.mail.ru>
2014-09-25 15:59:52 +0400

    listview adapter

--- src/tp/emazurov/lesson1/fragments/NewsListFragment.java
+++ src/tp/emazurov/lesson1/fragments/NewsListFragment.java
@@ -3,33 +3,30 @@ package tp.emazurov.lesson1.fragments;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import tp.emazurov.lesson1.R;
 
 /**
  * Created by r.kildiev on 11.09.2014.
  */
-public class NewsListFragment extends ListFragment {
+public class NewsListFragment extends Fragment {
     String[] numbers_text = new String[]{"one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine", "ten", "eleven",
             "twelve", "thirteen", "fourteen", "fifteen"};
     private OnItemSelectedListener mCallback;
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        mCallback.onArticleSelected(position);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,
-                numbers_text);
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.news_list_fargment, null);
+
     }
 
     @Override
@@ -46,8 +43,49 @@ public class NewsListFragment extends ListFragment {
         }
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView list = (ListView) view.findViewById(R.id.list);
+        list.setAdapter(new NewsListAdapter(numbers_text));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                mCallback.onArticleSelected(position);
+            }
+        });
+    }
+
     // Container Activity must implement this interface
     public interface OnItemSelectedListener {
         public void onArticleSelected(int position);
+    }
+
+    private class NewsListAdapter extends ArrayAdapter<String> {
+
+        public NewsListAdapter(String[] objects) {
+            super(getActivity(), 0, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.text = (TextView) convertView.findViewById(R.id.text);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.text.setText(getItem(position));
+            return convertView;
+        }
+
+        private class ViewHolder {
+
+            public TextView text;
+        }
     }
 }
