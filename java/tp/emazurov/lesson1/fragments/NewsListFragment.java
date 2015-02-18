420857ff040f3bdcc03c2501222616e649ddb804
Rustam Kildiev <kildievr@gmail.com>
2015-02-19 00:28:33 +0300

    gradle fix

--- java/tp/emazurov/lesson1/fragments/NewsListFragment.java
+++ java/tp/emazurov/lesson1/fragments/NewsListFragment.java
@@ -26,7 +26,7 @@ public class NewsListFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1,
                 numbers_text);
         setListAdapter(adapter);
         return super.onCreateView(inflater, container, savedInstanceState);
